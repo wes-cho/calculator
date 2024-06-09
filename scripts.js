8a38a85624e78340b852910bbe2b479cd3027e5f
Wesley Cho <57739900+wes-cho@users.noreply.github.com>
2024-06-09 18:34:15 -0400

    Added ability to calculate continuously
    User can calculate continuously without having to
    click on the actual calculate button. Also
    includes conditionals to prevent unwanted
    calculations from occurring if they were to click
    on an operator and/or calculate button multiple
    times before clicking a number.

--- scripts.js
+++ scripts.js
@@ -30,21 +30,23 @@ function operate(num1, operator, num2){
 let number1 = '';
 let number2 = '';
 let operator = '';
-let display = '';
-let result = '';
 let previousClicks = [];
 
 const clearButton = document.querySelector(".clear");
-clearButton.addEventListener('click', (e) => {
+clearButton.addEventListener('click', () => {
     document.querySelector('.display').textContent = '';
     previousClicks = [];
+    number1 = '';
+    number2 = '';
+    
 });
 
 const numberButtons = document.querySelectorAll('#number');
 for (let i=0; i<numberButtons.length; i++){
-    numberButtons[i].addEventListener('click', (e) => {
+    numberButtons[i].addEventListener('click', () => {
         previousClicks.push(numberButtons[i].id);
         document.querySelector('.display').textContent += numberButtons[i].textContent;
+        number2 = document.querySelector('.display').textContent;
         if (previousClicks[previousClicks.length-2] == 'operator'){
             document.querySelector('.display').textContent = numberButtons[i].textContent;
         };
@@ -53,16 +55,21 @@ for (let i=0; i<numberButtons.length; i++){
 
 const operatorButtons = document.querySelectorAll('#operator');
 for (let i=0; i<operatorButtons.length; i++){
-    operatorButtons[i].addEventListener('click', (e) => {
+    operatorButtons[i].addEventListener('click', () => {
+        if (number1 && operator && previousClicks[previousClicks.length-1] != 'operator'){
+            number2 = document.querySelector('.display').textContent;
+            document.querySelector('.display').textContent = operate(number1, operator, number2);
+        };
         previousClicks.push(operatorButtons[i].id);
-        operator = operatorButtons[i].textContent;
         number1 = document.querySelector('.display').textContent;
+        operator = operatorButtons[i].textContent;
     });
 };
 
 const decimalButton = document.querySelector('#decimal');
-decimalButton.addEventListener('click', (e) => {
+decimalButton.addEventListener('click', () => {
     previousClicks.push(decimalButton.id);
+    number2 = document.querySelector('.display').textContent;
     if (document.querySelector('.display').textContent.includes('.')){
         document.querySelector('.display').textContent += '';
     } else {
@@ -73,5 +80,6 @@ decimalButton.addEventListener('click', (e) => {
 const calculateButton = document.querySelector('#equals');
 calculateButton.addEventListener('click', () => {
     number2 = document.querySelector('.display').textContent;
-    document.querySelector('.display') = operate(number1, operator, number2);
+    document.querySelector('.display').textContent = operate(number1, operator, number2);
+    number1 = '';
 });
