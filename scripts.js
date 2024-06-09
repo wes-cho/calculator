4b6bf0aa2d3ac7907a9e28d90ffbda7000d792d6
Wesley Cho <57739900+wes-cho@users.noreply.github.com>
2024-06-09 18:47:37 -0400

    Adds helpful comments for readability
    Comments have been added where necessary to help
    myself or other developers better understand WHY
    certain code has been written.

--- scripts.js
+++ scripts.js
@@ -30,6 +30,7 @@ function operate(num1, operator, num2){
 let number1 = '';
 let number2 = '';
 let operator = '';
+//holds list of what buttons the user has clicked (number/operator/decimal)
 let previousClicks = [];
 
 const clearButton = document.querySelector(".clear");
@@ -45,8 +46,11 @@ const numberButtons = document.querySelectorAll('#number');
 for (let i=0; i<numberButtons.length; i++){
     numberButtons[i].addEventListener('click', () => {
         previousClicks.push(numberButtons[i].id);
+        //updates the calculator's display with the number clicked
         document.querySelector('.display').textContent += numberButtons[i].textContent;
+        //ensures var number2 is updated whenever a number is clicked
         number2 = document.querySelector('.display').textContent;
+        //clears display when a number is clicked following an operator
         if (previousClicks[previousClicks.length-2] == 'operator'){
             document.querySelector('.display').textContent = numberButtons[i].textContent;
         };
@@ -56,6 +60,8 @@ for (let i=0; i<numberButtons.length; i++){
 const operatorButtons = document.querySelectorAll('#operator');
 for (let i=0; i<operatorButtons.length; i++){
     operatorButtons[i].addEventListener('click', () => {
+        //runs calculation if number1 and operator have both been set, and last click wasn't an operator
+        // also updates the display accordingly
         if (number1 && operator && previousClicks[previousClicks.length-1] != 'operator'){
             number2 = document.querySelector('.display').textContent;
             document.querySelector('.display').textContent = operate(number1, operator, number2);
@@ -69,7 +75,9 @@ for (let i=0; i<operatorButtons.length; i++){
 const decimalButton = document.querySelector('#decimal');
 decimalButton.addEventListener('click', () => {
     previousClicks.push(decimalButton.id);
+    //ensures var number2 is updated whenever a decimal is clicked
     number2 = document.querySelector('.display').textContent;
+    //checks if number on display already contains a decimal -- adds one if it doesn't
     if (document.querySelector('.display').textContent.includes('.')){
         document.querySelector('.display').textContent += '';
     } else {
@@ -81,5 +89,6 @@ const calculateButton = document.querySelector('#equals');
 calculateButton.addEventListener('click', () => {
     number2 = document.querySelector('.display').textContent;
     document.querySelector('.display').textContent = operate(number1, operator, number2);
+    //clears variable so that user can't continuously click "calculate" 
     number1 = '';
 });
